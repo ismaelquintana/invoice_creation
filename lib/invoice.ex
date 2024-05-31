cc8ddb20a188422e716441b158501d926b9f1856
ismael <ismael.quintana@proton.me>
2024-05-31 15:15:02 +0200

    Funtions to create structs

--- lib/invoice.ex
+++ lib/invoice.ex
@@ -10,5 +10,14 @@ defmodule Invoice do
             sale_amount: nil,
             vat: nil
 
-  def new(), do: %Invoice{}
+  def new() do
+    date = Date.utc_today()
+    number = "#{date.year}-0000"
+
+    %Invoice{
+      date: date,
+      number: number,
+      concepts: [%Bill{invoice_number: number}]
+    }
+  end
 end
