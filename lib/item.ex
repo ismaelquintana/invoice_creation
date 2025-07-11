f6ab3b2c6faabb02bda71d0e1fe51799a025235a
ismael <ismael.quintana@proton.me>
2025-07-11 10:51:02 +0200

    eliminacion de comentarios de script

--- lib/item.ex
+++ lib/item.ex
@@ -17,11 +17,6 @@ defmodule Item do
     struct(__MODULE__, opts)
   end
 
-  # @spec new :: Item.t()
-  # def new do
-  #   %Item{}
-  # end
-
   @spec update(Item.t(), keyword()) :: Item.t()
   def update(item, opts) do
     struct(item, opts)
